bb365c23be10c1debad88763dd5a910206483071
YassineGallaoui <myassine.gallaoui@gmail.com>
2025-02-13 13:05:57 +0100

    update asset manifest and enhance Vite configuration for multiple entry points

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import postcssHash from 'postcss-hash';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: 'src',
     build: {
         outDir: '../dist',
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                about: resolve(__dirname, 'src/about.html'),
+            },
+        },
+
     },
     css: {
         preprocessorOptions: {
