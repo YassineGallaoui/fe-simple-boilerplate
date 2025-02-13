71af527b3edf1463d06f90a3f92266727a48d9c5
YassineGallaoui <myassine.gallaoui@gmail.com>
2025-02-13 12:02:31 +0100

    refactor styles and enhance layout responsiveness; update SCSS files and add position mixins

--- src/js/helpers/grid.js
+++ src/js/helpers/grid.js
@@ -3,16 +3,7 @@ export function initGrid() {
     const overlay = document.querySelector('.grid-overlay');
     let columnCount = getComputedStyle(overlay).getPropertyValue('--columns');
 
-    window.addEventListener('resize', (e) => {
-        columnCount = getComputedStyle(overlay).getPropertyValue('--columns');
-        overlay.innerHTML = ""
-        for (let i = 0; i < columnCount; i++) {
-            const col = document.createElement('div');
-            overlay.appendChild(col);
-        }
-    });
-
-    document.addEventListener('keydown', (e) => {
+    const keydownHandler = (e) => {
         if (e.key.toLowerCase() === 'g') {
             overlay.classList.toggle('show')
             overlay.innerHTML = ""
@@ -21,5 +12,33 @@ export function initGrid() {
                 overlay.appendChild(col);
             }
         }
-    });
+    };
+    const loadHandler = () => {
+        if (overlay.classList.contains('show')) {
+            overlay.innerHTML = ""
+            for (let i = 0; i < columnCount; i++) {
+                const col = document.createElement('div');
+                overlay.appendChild(col);
+            }
+        }
+    };
+    const resizeHandler = (e) => {
+        columnCount = getComputedStyle(overlay).getPropertyValue('--columns');
+        overlay.innerHTML = ""
+        for (let i = 0; i < columnCount; i++) {
+            const col = document.createElement('div');
+            overlay.appendChild(col);
+        }
+    };
+
+
+    window.addEventListener('resize', (e) => resizeHandler(e));
+    window.addEventListener('load', (e) => loadHandler(e));
+    document.addEventListener('keydown', (e) => keydownHandler(e));
+
+    return () => {
+        window.removeEventListener('resize', resizeHandler);
+        window.removeEventListener('load', loadHandler);
+        document.removeEventListener('keydown', keydownHandler);
+    };
 }
--- src/js/helpers/stats.js
+++ src/js/helpers/stats.js
@@ -1,11 +1,18 @@
 // src/js/grid.js
+
+const getCurrentInfo = () => {
+    return `
+        <div>${window.innerWidth} x ${window.innerHeight}</div>
+        <div>Aspect Ratio: ${(window.innerWidth / window.innerHeight).toFixed(2)}</div>
+    `;
+}
 export function initStats() {
     const statsContainer = document.querySelector('#stats');
     if (statsContainer) {
-        statsContainer.innerHTML = `${window.innerWidth} x ${window.innerHeight}`
+        statsContainer.innerHTML = getCurrentInfo();
 
-        window.addEventListener('resize', (e) => {
-            statsContainer.innerHTML = `${window.innerWidth} x ${window.innerHeight}`
+        window.addEventListener('resize', () => {
+            statsContainer.innerHTML = getCurrentInfo();
         });
 
         document.addEventListener('keydown', (e) => {
