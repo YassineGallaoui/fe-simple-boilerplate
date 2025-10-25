8eb76781a700954a64debba8df2d2df86ce0db9d
YassineGallaoui <yassine.gallaoui.dev@gmail.com>
2025-10-25 22:23:14 +0200

    feat: restructure HTML headers to use nav elements and add footer with links

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,7 @@
 // Import global styles
 import 'msccss/dist/main.min.css';
 import { grid, router, stats } from 'yg-vanilla-js-helpers';
+import '../styles/common.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   router.init();
