1192b7d731623404c6551ee09f91559e250bda61
YassineGallaoui <yassine.gallaoui.dev@gmail.com>
2026-01-12 12:22:58 +0100

    feat: updated yg-vanilla-js-helpers

--- src/js/common.js
+++ src/js/common.js
@@ -5,6 +5,12 @@ import '/node_modules/msccss/dist/main.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   router.init();
-  grid.init(true);
+  grid.init({
+    show: true,
+    columnsColor: 'rgba(255, 0, 0, 0.1)',
+    columnsBorderColor: 'rgba(238, 0, 0, 0.6)',
+    columnsBorderWidth: '1px',
+    columnsBorderStyle: 'dashed',
+  });
   stats.init();
 })
